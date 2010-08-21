dc3e782460b96d2ab75ae22acc5f4e51a932d23d
Sam Warmuth <sam@samwarmuth.com>
2010-08-21 17:56:29 -0500

    basics

--- pack.rb
+++ pack.rb
@@ -1,11 +1,13 @@
-total_boxes = gets.to_i
+number_of_boxies = gets.to_i
 return "Not an int" if total_boxes.nil?
 puts "Boxes: #{total_boxes}"
+boxes = []
 total_area = 0
-total_boxes.times do
+number_of_boxes.times do
   a , b = gets.split(" ").map{|n| n.to_i}
   return "input error" if (a.nil? || b.nil?)
   area = a*b
   puts "Box: #{a} x #{b} - #{area}u : #{total_area += area}u total"
-  
+  boxes << [a,b]
 end
+inspect boxes
